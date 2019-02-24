d84351e9d4d9bf3c20103d0dc49556350189c7a0
Glen Ashwood <glenashwood@glens-mbp.home>
2019-02-24 22:46:32 +0000

    update jenkins script

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -30,5 +30,5 @@ sudo docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_ho
 
 # show endpoint
 echo 'Jenkins installed'
-echo 'You should now be able to access jenkins at: http://'$(curl -s ifconfig.co)':8080'mkdir -p /var/jenkins_home
+echo 'You should now be able to access jenkins at: http://'$(curl -s ifconfig.co)':8080'
 
