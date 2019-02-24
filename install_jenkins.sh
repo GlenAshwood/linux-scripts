da75d3265a6e92b158cda726c9b639a861fc67d9
Glen Ashwood <glenashwood@glens-mbp.home>
2019-02-24 22:32:29 +0000

    add jenkins script

---
+++ install_jenkins.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# this script is only tested on ubuntu xenial
+
+# install docker
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+#sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo systemctl enable docker
+sudo systemctl start docker
+
+#Run Jenkins
+sudo mkdir -p /var/jenkins_home
+sudo chown -R 1000:1000 /var/jenkins_home/
+sudo docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -d --name jenkins jenkins/jenkins:lts
+
+# show endpoint
+echo 'Jenkins installed'
+echo 'You should now be able to access jenkins at: http://'$(curl -s ifconfig.co)':8080'mkdir -p /var/jenkins_home
+
