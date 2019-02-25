c12b8ca7073ab66b70cc6dcb53485fb1989da9e5
Glen Ashwood <glenashwood@glens-mbp.home>
2019-02-25 21:09:53 +0000

    add restart policy

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -26,7 +26,7 @@ sudo systemctl start docker
 #Run Jenkins
 sudo mkdir -p /var/jenkins_home
 sudo chown -R 1000:1000 /var/jenkins_home/
-sudo docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -d --name jenkins jenkins/jenkins:lts
+sudo docker run -p 8080:8080 -p 50000:50000 --restart=always -v /var/jenkins_home:/var/jenkins_home -d --name jenkins jenkins/jenkins:lts
 
 # show endpoint
 echo 'Jenkins installed'
